303ad20cf658f73dd6533aaa0cd5fa5e66a3346f
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2024-02-18 14:02:47 +0200

    new django project upload

--- petstragram/settings.py
+++ petstragram/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #project apps
+    'petstragram.pets',
+    'petstragram.photos',
+    'petstragram.common',
+    'petstragram.accounts',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
