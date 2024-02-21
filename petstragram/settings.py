2c3750cec24a902a983a9e51c92c4fba423e7234
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2024-02-21 22:52:16 +0200

    new django project upload

--- petstragram/settings.py
+++ petstragram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ggaz43o8wc*12d&9ai+5!j^)b%c(!^3_xqx!+%(uu9my&=hhm4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
